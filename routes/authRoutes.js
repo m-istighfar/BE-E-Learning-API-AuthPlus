e8f86b621452a2ebfd1783c5c3a157313b07706d
Tifar <istighfar.amal@gmail.com>
2023-10-13 17:46:56 +0700

    add login limiter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,20 +4,27 @@ const AuthController = require("../controllers/AuthController");
 const PasswordResetController = require("../controllers/PasswordResetController");
 const { rateLimit } = require("express-rate-limit");
 
-const MemoryStore = rateLimit.MemoryStore;
+const UserRateLimitStore = require("../routes/UserRateLimitStore");
 
-const store = new MemoryStore();
+const windowMs = 15 * 60 * 1000;
 
-const loginLimiter = rateLimit({
-  store: store,
-  windowMs: 15 * 60 * 1000,
+const userRateLimitStoreInstance = new UserRateLimitStore(windowMs);
+
+const userLoginLimiter = rateLimit({
+  store: userRateLimitStoreInstance,
+  windowMs: windowMs,
   max: 5,
   skipSuccessfulRequests: true,
   message: "Too many failed login attempts, please try again after 15 minutes.",
+  keyGenerator: (req) => {
+    const key = req.body.username;
+    console.log(`Generating key for rate limiter: ${key}`);
+    return key;
+  },
 });
 
-router.post("/login", loginLimiter, AuthController.login);
-router.post("/login-session", loginLimiter, AuthController.loginWihSession);
+router.post("/login", userLoginLimiter, AuthController.login);
+router.post("/login-session", userLoginLimiter, AuthController.loginWihSession);
 
 router.post("/register", AuthController.register);
 router.post("/refreshToken", AuthController.refreshTokenHandler);
@@ -33,5 +40,27 @@ router.post(
   "/reset-password/:resetToken",
   PasswordResetController.resetPassword
 );
+
+router.get("/rate-limit-data", (req, res) => {
+  const storeData = userRateLimitStoreInstance.getAllData();
+
+  res.json(storeData);
+});
+
+router.post("/rate-limit-reset/:key", async (req, res) => {
+  const { key } = req.params;
+  const keyWasReset = await userRateLimitStoreInstance.resetKey(key);
+
+  if (keyWasReset) {
+    res.json({ message: `Key ${key} reset.` });
+  } else {
+    res.status(404).json({ message: `Key ${key} not found.` });
+  }
+});
+
+router.post("/rate-limit-reset-all", (req, res) => {
+  userRateLimitStoreInstance.resetAll();
+  res.send({ message: "All rate limits reset" });
+});
 
 module.exports = router;
